c192ae4dbb0e7c897687306d1dfc626c1b1bc098
sourabh <sourabhsisodia010@gmail.com>
2023-09-02 13:35:29 +0530

    completed flights functionality

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -14,4 +14,7 @@ router.delete("/city/:id", CityController.destroy);
 router.patch("/city/:id", CityController.update);
 
 router.post("/flights", FlightController.create);
+router.get("/flights", FlightController.getAll);
+router.get("/flights/:id", FlightController.get);
+router.patch("/flights/:id", FlightController.update);
 module.exports = router;
